94ec3896c7eda8cee0d46c0d76ed801ded736b11
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-11-30 18:04:57 -0600

    finish first part calc

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+from operations import add, sub, mult, div
 
+app = Flask(__name__)
+
+@app.get('/add')
+def addition():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return f"<html><body><h1>{add(a,b)}</h1></body></html>"
+
+@app.get('/sub')
+def subtraction():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return f"<html><body><h1>{sub(a,b)}</h1></body></html>"
+
+@app.get('/mult')
+def multiply():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return f"<html><body><h1>{mult(a,b)}</h1></body></html>"
+
+@app.get('/div')
+def divide():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    return f"<html><body><h1>{div(a,b)}</h1></body></html>"
 
